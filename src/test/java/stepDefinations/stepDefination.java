543d75af102640b60b2128a9542a2a170b7819d0
rupam9753 <rupampramanick007@gmail.com>
2021-07-24 22:33:39 +0530

    Architect Asian Development

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -25,6 +25,7 @@ public void initialize_the_browser_with_chrome() throws Throwable {
 public void navigate_to_Site(String arg1) throws Throwable {
     // Write code here that turns the phrase above into concrete actions
 	driver.get(arg1);
+	System.out.println("Here's another change");
 }
 
 @And("^click on Login link in home page to land on Secure sign in page$")
@@ -48,6 +49,7 @@ public void verify_that_user_is_successfully_logged_in() throws Throwable {
     // Write code here that turns the phrase above into concrete actions
     portalHomePage p=new portalHomePage(driver);
     Assert.assertTrue(p.getqueryTextbox().isDisplayed());
+    System.out.println("Change for Dev");
 }
 
 }
